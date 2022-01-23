f51feda8dbc84f80fa974086102dfe2f4446e3c6
zhenlong.su <794316342@qq.com>
2022-01-23 16:05:24 +0800

    主要功能完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,26 @@
+import React from 'react'
 import { Redirect } from 'react-router-dom'
-import ALDiscover from '@/pages/discover'
-import ALFriend from '@/pages/friend'
-import ALMine from '@/pages/mine'
-import ALRecommend from '@/pages/discover/c-pages/recommend'
-import ALRanking from '@/pages/discover/c-pages/ranking'
-import ALSongs from '@/pages/discover/c-pages/songs'
-import ALDjradio from '@/pages/discover/c-pages/djradio'
-import ALArtist from '@/pages/discover/c-pages/artist'
-import ALAlbum from '@/pages/discover/c-pages/album'
-import ALPlayer from '@/pages/player'
+// import ALDiscover from '@/pages/discover'
+// import ALFriend from '@/pages/friend'
+// import ALMine from '@/pages/mine'
+// import ALRecommend from '@/pages/discover/c-pages/recommend'
+// import ALRanking from '@/pages/discover/c-pages/ranking'
+// import ALSongs from '@/pages/discover/c-pages/songs'
+// import ALDjradio from '@/pages/discover/c-pages/djradio'
+// import ALArtist from '@/pages/discover/c-pages/artist'
+// import ALAlbum from '@/pages/discover/c-pages/album'
+// import ALPlayer from '@/pages/player'
+
+const ALDiscover = React.lazy(() => import('@/pages/discover'))
+const ALFriend = React.lazy(() => import('@/pages/friend'))
+const ALMine = React.lazy(() => import('@/pages/mine'))
+const ALRecommend = React.lazy(() => import('@/pages/discover/c-pages/recommend'))
+const ALRanking = React.lazy(() => import('@/pages/discover/c-pages/ranking'))
+const ALSongs = React.lazy(() => import('@/pages/discover/c-pages/songs'))
+const ALDjradio = React.lazy(() => import('@/pages/discover/c-pages/djradio'))
+const ALArtist = React.lazy(() => import('@/pages/discover/c-pages/artist'))
+const ALAlbum = React.lazy(() => import('@/pages/discover/c-pages/album'))
+const ALPlayer = React.lazy(() => import('@/pages/player'))
 
 const routes = [
     {
